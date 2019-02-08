b56421d0e8e420ad02a96fd66bc1cd6aa4b00403
sungvadan <svd.phan@gmail.com>
2019-02-08 11:17:39 +0100

    add states on boards

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 
 
 class Square extends React.Component {
+    // TODO: remove the constructor
     constructor(props) {
         super(props);
         this.state = {
@@ -12,11 +13,10 @@ class Square extends React.Component {
     }
 
     render() {
+        // TODO: use onClick={this.props.onClick}
+        // TODO: replace this.state.value with this.props.value
         return (
-            <button
-                className="square"
-                onClick={() => this.setState({value: 'X'})}
-            >
+            <button className="square" onClick={() => this.setState({value: 'X'})}>
                 {this.state.value}
             </button>
         );
@@ -24,8 +24,15 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+
     renderSquare(i) {
-        return <Square />;
+        return <Square value={this.state.squares[i]} />;
     }
 
     render() {
